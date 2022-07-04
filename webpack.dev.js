2938c6b5b38996e0114a07b0f232b5ea7e4e440d
LucasReiche <lucas.reiche@mercedes-benz.com>
2022-07-04 15:26:54 +0200

    Fix webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -26,9 +27,31 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+    //
+    // default: false
+    dry: true,
+
+    // Write Logs to Console
+    // (Always enabled when dry is true)
+    //
+    // default: false
+    verbose: true,
+
+    // Automatically remove all unused webpack assets on rebuild
+    //
+    // default: true
+    cleanStaleWebpackAssets: false,
+
+    // Do not allow removal of current webpack assets
+    //
+    // default: true
+    protectWebpackAssets: false,
         })
     ],
     devServer: {
-        port: 8083,
+        port: 8081,
     },
 }
